f47af7571a680acbf8a7a8a1ae77b7bf404c22cb
Thanga Ganapathy <ganapathy888@gmail.com>
2019-05-16 15:58:20 +0530

    Show header default to all commands

--- src/cmdFlagInfo.js
+++ src/cmdFlagInfo.js
@@ -1,21 +1,19 @@
-const header = require('./header');
 const usage = require('./usage');
 const error = require('./utils/error');
+const { version } = require('../package.json');
 
 module.exports = (cmd) => {
   switch (cmd) {
     case '':
     case '-h':
     case '--help':
-      header();
       usage();
       break;
     case '-v':
     case '--version':
-      header();
+      console.log(`v${version}`);
       break;
     default:
-      header();
       error(`"${cmd}" is not a valid command!`);
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+const header = require('./header');
 const cmdFlagInfo = require('./cmdFlagInfo');
 
 module.exports = () => {
+  header();
   const cmd = (process.argv.slice(2)[0] || '').trim();
 
   if (cmd === '' || cmd.charAt(0) === '-') {
