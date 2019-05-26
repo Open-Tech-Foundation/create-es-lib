a588dffeb37f9790e2fd89609e3e2d33b2e32e05
Thanga Ganapathy <ganapathy888@gmail.com>
2019-05-26 12:37:20 +0530

    Add missing test for lib creations

--- __tests__/createLib.test.js
+++ __tests__/createLib.test.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const { existsSync } = require('fs');
 const { tmpdir } = require('os');
+const globby = require('globby');
 
 const createLib = require('../lib/generator/createLib');
 
@@ -17,27 +18,33 @@ describe('When invalid params passed', () => {
 });
 
 describe('When valid params passed', () => {
-  it('creates a cli lib', async () => {
-    expect.assertions(1);
+  it('creates a cli lib with no license', async () => {
+    expect.assertions(3);
     const projectPath = `${path.join(tmpdir(), 'cool-lib-cli')}`;
     const params = {
       libType: 'CLI',
       pkgName: 'cool-lib-cli',
       pkgManager: { cmd: 'npm', exe: 'npx' },
+      license: '',
     };
     await createLib(projectPath, params);
     expect(existsSync(projectPath)).toBeTruthy();
+    const files = await globby([projectPath], { dot: true });
+    expect(files.length).toBeGreaterThan(0);
+    expect(existsSync(path.join(projectPath, 'LICENSE'))).toBeFalsy();
   });
 
-  it('creates a module lib', async () => {
-    expect.assertions(1);
+  it('creates a module lib with MIT license', async () => {
+    expect.assertions(2);
     const projectPath = `${path.join(tmpdir(), 'cool-lib-mod')}`;
     const params = {
       libType: 'module',
       pkgName: 'cool-lib-mod',
       pkgManager: { cmd: 'npm', exe: 'npx' },
+      license: 'MIT',
     };
     await createLib(projectPath, params);
     expect(existsSync(projectPath)).toBeTruthy();
+    expect(existsSync(path.join(projectPath, 'LICENSE'))).toBeTruthy();
   });
 });
