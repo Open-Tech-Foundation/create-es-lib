53b78b07174bdbea580cd27c3837473767f32d8f
Thanga Ganapathy <ganapathy888@gmail.com>
2019-06-15 02:10:13 +0530

    Add license path to copy file patterns

--- __tests__/createLib.test.js
+++ __tests__/createLib.test.js
@@ -4,8 +4,16 @@ const globby = require('globby');
 const { tmpdir } = require('os');
 const rmfr = require('rmfr');
 
-const createLib = require('../lib/createLib');
-const devPkgs = require('../lib/createLib/devPkgs');
+const createLib = require('../lib/createLib/compileToCopyFiles');
+const devPkgs = require('../lib/installDevDeps/devPkgs');
+
+const cliProjectPath = path.join(tmpdir(), 'cool-lib-cli');
+const modProjectPath = path.join(tmpdir(), 'cool-lib-mod');
+
+async function cleanUp() {
+  await rmfr(cliProjectPath);
+  await rmfr(modProjectPath);
+}
 
 describe('When invalid params passed', () => {
   test('the empty params fails with an error', async () => {
@@ -22,52 +30,52 @@ describe('When invalid params passed', () => {
 });
 
 describe('When valid params passed', () => {
-  afterEach(async () => {
-    await rmfr(path.join(tmpdir(), 'cool-lib-cli'));
-    await rmfr(path.join(tmpdir(), '   cool-lib-mod'));
+  beforeEach(() => {
+    return cleanUp();
+  });
+
+  afterEach(() => {
+    return cleanUp();
   });
 
   it('creates a cli lib with no license', async () => {
     expect.assertions(3);
-    const projectPath = path.join(tmpdir(), 'cool-lib-cli');
     const params = {
       libType: 'CLI',
       pkgName: 'cool-lib-cli',
       pkgManager: { cmd: 'npm', exe: 'npx' },
       license: '',
     };
-    await createLib(projectPath, params);
-    expect(existsSync(projectPath)).toBeTruthy();
-    const files = await globby([projectPath], { dot: true });
+    await createLib(cliProjectPath, params);
+    expect(existsSync(cliProjectPath)).toBeTruthy();
+    const files = await globby([cliProjectPath], { dot: true });
     expect(files.length).toBeGreaterThan(0);
-    expect(existsSync(path.join(projectPath, 'LICENSE'))).toBeFalsy();
+    expect(existsSync(path.join(cliProjectPath, 'LICENSE'))).toBeFalsy();
   }, 15000);
 
   it('creates a module lib with MIT license', async () => {
     expect.assertions(2);
-    const projectPath = path.join(tmpdir(), 'cool-lib-mod');
     const params = {
       libType: 'module',
       pkgName: 'cool-lib-mod',
       pkgManager: { cmd: 'npm', exe: 'npx' },
       license: 'MIT',
     };
-    await createLib(projectPath, params);
-    expect(existsSync(projectPath)).toBeTruthy();
-    expect(existsSync(path.join(projectPath, 'LICENSE'))).toBeTruthy();
+    await createLib(modProjectPath, params);
+    expect(existsSync(modProjectPath)).toBeTruthy();
+    expect(existsSync(path.join(modProjectPath, 'LICENSE'))).toBeTruthy();
   }, 15000);
 
   test('npm to install cli lib dev deps', async () => {
     expect.assertions(1);
-    const projectPath = path.join(tmpdir(), 'cool-lib-cli');
     const params = {
       libType: 'CLI',
       pkgName: 'cool-lib-cli',
       pkgManager: { cmd: 'npm', exe: 'npx' },
     };
-    await createLib(projectPath, params);
+    await createLib(cliProjectPath, params);
 
-    const rawdata = readFileSync(path.join(projectPath, 'package.json'));
+    const rawdata = readFileSync(path.join(cliProjectPath, 'package.json'));
     const packageData = JSON.parse(rawdata);
     expect(Object.keys(packageData.devDependencies)).toEqual([
       ...devPkgs.common,
@@ -77,15 +85,14 @@ describe('When valid params passed', () => {
 
   test('yarn to install module lib dev deps', async () => {
     expect.assertions(1);
-    const projectPath = path.join(tmpdir(), 'cool-lib-mod');
     const params = {
       libType: 'module',
       pkgName: 'cool-lib-mod',
       pkgManager: { cmd: 'yarn', exe: 'yarn' },
     };
-    await createLib(projectPath, params);
+    await createLib(modProjectPath, params);
 
-    const rawdata = readFileSync(path.join(projectPath, 'package.json'));
+    const rawdata = readFileSync(path.join(modProjectPath, 'package.json'));
     const packageData = JSON.parse(rawdata);
     expect(Object.keys(packageData.devDependencies)).toEqual([
       ...devPkgs.common,
