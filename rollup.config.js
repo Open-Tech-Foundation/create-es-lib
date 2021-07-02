0251d9ef7d297ba46911e08e5c5dc3b3ad0506e0
Thanga Ganapathy <ganapathy888@gmail.com>
2021-07-02 20:29:37 +0530

    Copy templates to build

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import typescript from '@rollup/plugin-typescript';
 import clean from '@open-tech-world/rollup-plugin-clean';
+import copy from 'rollup-plugin-copy';
 
 export default {
   input: 'src/index.ts',
@@ -8,7 +9,14 @@ export default {
     format: 'cjs',
     exports: 'auto',
   },
-  plugins: [clean({ start: 'lib/**' }), typescript()],
+  plugins: [
+    clean({ start: 'lib/**' }),
+    typescript(),
+    copy({
+      targets: [{ src: 'src/templates', dest: 'lib' }],
+      copyOnce: true,
+    }),
+  ],
   external: [
     'chalk',
     'yargs',
