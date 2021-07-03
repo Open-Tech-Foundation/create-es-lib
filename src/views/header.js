7d8a901eb422af5deac2231edb3f39ef2d36f6c9
Thanga Ganapathy <ganapathy888@gmail.com>
2021-07-03 12:03:17 +0530

    Remove unwanted views files

--- src/views/header.js
+++
@@ -1,7 +0,0 @@
-const chalk = require('chalk');
-const { version } = require('../../package.json');
-
-module.exports = () => {
-  const header = chalk`\n\t{bold.rgb(255, 136, 0) create-node-lib} {gray [v${version}]} \n`;
-  console.log(header);
-};
--- src/views/usage.js
+++
@@ -1,9 +0,0 @@
-const chalk = require('chalk');
-
-module.exports = () => {
-  const usage = chalk` {bold.cyan Usage:}
-
-    $ npx @open-tech-world/create-node-lib <your-project-name>
-  `;
-  console.log(usage);
-};
