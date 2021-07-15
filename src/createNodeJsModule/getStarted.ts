84e68d4113ce9ba6d7b031a86bea4447cd7ae740
Thanga Ganapathy <ganapathy888@gmail.com>
2021-07-15 11:51:12 +0530

    Update getting started info

--- src/createNodeJsModule/getStarted.ts
+++ src/createNodeJsModule/getStarted.ts
@@ -13,17 +13,19 @@ export default async function getStarted(config: IConfig): Promise<void> {
     console.log('Run the following commands to get started 🚀');
     console.log('');
     console.log(chalk.blueBright(`\t$ cd ${config.libName}`));
-    console.log('');
+    console.log('\t - Change directory');
     console.log(
       chalk.blueBright(`\t$ ${getPkgManagerRunCmd(config.pkgManager)} build`)
     );
-    console.log('\t If you enabled a bundler, then this will build your lib.');
+    console.log(
+      '\t - If you enabled a bundler, then this will build your lib.'
+    );
     console.log('');
     console.log(
       chalk.blueBright(`\t$ ${getPkgManagerRunCmd(config.pkgManager)} test`)
     );
     console.log(
-      '\t If you enabled a test runner, then this will run your tests.'
+      '\t - If you enabled a test runner, then this will run your tests.'
     );
     console.log('');
     resolve();
