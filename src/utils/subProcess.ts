33ebca7fea6e8ed7a1977ddcd1d997fbd1af290d
Thanga Ganapathy <ganapathy888@gmail.com>
2021-07-15 21:34:59 +0530

    Add debug to subprocess util

--- src/utils/subProcess.ts
+++ src/utils/subProcess.ts
@@ -3,27 +3,38 @@ import { spawn, ChildProcess } from 'child_process';
 export default async function subProcess(
   cmd: string,
   args: string[],
-  cwd: string
+  cwd: string,
+  debug = false
 ): Promise<void> {
   let sp: ChildProcess;
 
   if (process.platform === 'win32') {
-    sp = spawn(cmd, args, { cwd, shell: 'cmd.exe', stdio: 'ignore' });
+    sp = spawn(cmd, args, {
+      cwd,
+      shell: 'cmd.exe',
+      stdio: debug ? 'inherit' : 'ignore',
+    });
   } else {
-    sp = spawn(cmd, args, { cwd, shell: true, stdio: 'ignore' });
+    sp = spawn(cmd, args, {
+      cwd,
+      shell: true,
+      stdio: debug ? 'inherit' : 'ignore',
+    });
   }
 
   return new Promise((resolve, reject) => {
-    // if (s.stdout) {
-    //   s.stdout.on('data', (chunk) => {
-    //     console.log(chunk.toString());
-    //   });
-    // }
-    // if (s.stderr) {
-    //   s.stderr.on('data', (chunk) => {
-    //     console.log(chunk.toString());
-    //   });
-    // }
+    if (debug) {
+      if (sp.stdout) {
+        sp.stdout.on('data', (chunk) => {
+          console.log(chunk.toString());
+        });
+      }
+      if (sp.stderr) {
+        sp.stderr.on('data', (chunk) => {
+          console.log(chunk.toString());
+        });
+      }
+    }
     sp.on('close', (code) => {
       if (code && code > 0) {
         reject(code);
